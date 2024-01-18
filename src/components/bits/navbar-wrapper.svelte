f8e4cba45b16e3ed5d798156537314f618235641
Suleiman Ali <suleimanali.business@gmail.com>
2024-01-18 19:19:10 +0300

    home: add navbar

---
+++ src/components/bits/navbar-wrapper.svelte
@@ -0,0 +1,11 @@
+<script>
+  let scrollY;
+</script>
+
+<nav
+  class="px-3 w-full flex flex-col items-center fixed top-0 left-0 transition border-b border-b-transparent {scrollY >
+    0 && '!border-b-zinc-300'}"
+>
+  <slot />
+</nav>
+<svelte:window bind:scrollY />
